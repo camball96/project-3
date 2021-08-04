8057b94fd283dd931211739d576f0b06758c9084
Cam <cam.ball96@hotmail.com>
2021-08-04 18:04:56 +1000

    Linking our register page to our server to store new users. Running into issues with useMutation hook

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { Button, Checkbox, Container, Form, Grid, Dropdown, Menu } from 'semantic-ui-react'
+
+function submit(){
+    console.log("Submitted");
+};
+
 
 function Login() {
     return (
         <div>
-            <h1>Login Page</h1>
+            <h1>Log in and see what's new!?</h1>
+            <Grid celled>
+                <Grid.Row>
+                    <Grid.Column width={5}>
+                        <Menu secondary vertical>
+                            <Menu.Item
+                            name='account'
+                            //active={activeItem === 'account'}
+                            //onClick={this.handleItemClick}
+                            />
+                            <Menu.Item
+                            name='settings'
+                            //active={activeItem === 'settings'}
+                            //onClick={this.handleItemClick}
+                            />
+                            <Dropdown item text='Display Options'>
+                            <Dropdown.Menu>
+                                <Dropdown.Header>Text Size</Dropdown.Header>
+                                <Dropdown.Item>Small</Dropdown.Item>
+                                <Dropdown.Item>Medium</Dropdown.Item>
+                                <Dropdown.Item>Large</Dropdown.Item>
+                            </Dropdown.Menu>
+                            </Dropdown>
+                        </Menu>
+                    </Grid.Column>
+                    <Grid.Column width={10}>
+                        <Form onSubmit={submit} >
+                            <Form.Input label='Enter Username' type='username' name='username' />
+                            <Form.Input label='Enter Password' type='password' />
+                            <Button content='Register!' />
+                        </Form>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+
         </div>
     )
 }
--- client/src/Pages/register.js
+++ client/src/Pages/register.js
@@ -1,22 +1,85 @@
-import React from 'react'
+import React, { useState, useMutation } from 'react'
 import { Button, Checkbox, Container, Form } from 'semantic-ui-react'
+import gql from 'graphql-tag';
 
 
 function submit(){
     console.log("Submitted");
 };
 
-function Register() {
+
+
+// Make checkbox agreement save on db?
+function NewRegister() {
+    const [values, setValues] = useState({
+        username: '',
+        email: '',
+        password:'',
+        confirmPassword:''
+    })
+
+    const onChange = (e) => {
+        setValues({...values,[e.target.name]: e.target.value});
+    }
+    
+    
+    const [ addUser, { loading }] = useMutation(NEW_REGISTER,{
+        update(result){
+            console.log(result);
+        },
+        variables: {
+            username: values.username,
+            email: values.email,
+            password: values.password,
+            confirmPassword: values.confirmPassword
+        }
+    });
+
+    const onSub = (e) => {
+        e.preventDefault();
+        addUser();
+    }
+
+
+ 
     return (
         <div>
+            <h1>Ready to sign up?</h1>
             <Form onSubmit={submit} >
-            <Form.Input label='Enter Username' type='username' />
-            <Form.Input label='Enter Password' type='password' />
-            <Form.Input label='Enter Email' type='email' />
+            <Form.Input label='Enter Username' type='username' name='username' value={values.username} onChange={onChange}/>
+            <Form.Input label='Enter Password' type='password' value={values.password} onChange={onChange} />
+            <Form.Input label='Confirm Password' type='password' value={values.confirmPassword} onChange={onChange} />
+            <Form.Input label='Enter Email' type='email' value={values.email} onChange={onChange} />
+            <Form.Checkbox label='I agree to the Terms and Conditions' type='checkbox' name='checkbox'/>
+            <Button type='submit' content='Register!' />
             </Form>
         </div>
     )
 }
 
+const NEW_REGISTER = gql`
+    mutation register(
+        $username: String!
+        $email: String!
+        $password: String!
+        $confirmPassword: String!
+    ) {
+        register(
+            registerInput: {
+                username: $username
+                email: $email
+                password: $password
+                confirmPassword: $confirmPassword
 
-export default Register;
+            }
+        ){
+            id
+            email
+            createdAt 
+            username
+            token
+        }
+    }
+`
+
+export default NewRegister;
